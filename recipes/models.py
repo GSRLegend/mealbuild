18728d295f7ed3eee6a8cd7dfdc755769c213ced
GSRLegend <gsimpsonrister@gmail.com>
2026-05-14 19:17:18 -0400

    prepping for switch from hardcoded recipes to models

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,267 @@
 from django.db import models
 
 # Create your models here.
+class Recipe(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    serving_size = models.CharField(max_length=100)
+    image = models.ImageField(
+        upload_to='recipe_images/',
+        blank=True,
+        null=True
+    )
+    source = models.URLField(
+        blank=True,
+        null=True
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )  
+    def __str__(self):
+        return str(self.id) + ' - ' + self.name
+
+class RecipeSteps(models.Model):
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='steps'
+    )
+    step_number = models.PositiveIntegerField()
+    instruction = models.TextField()
+    class Meta:
+        ordering = ['step_number']
+
+    def __str__(self):
+        return f"{self.recipe.name} - Step {self.step_number}"
+
+class Ingredient:
+    name = models.CharField(
+        max_lenght=100,
+        unique=True
+    )
+
+    #Macros per 100g
+    #-------------------------------------
+    calories = models.FloatField(default=0)
+    fat = models.FloatField(default=0)                  #g
+    protein = models.FloatField(default=0)              #g
+    carbohydrates = models.FloatField(default=0)        #g
+    cholesterol = models.FloatField(default=0)          #mg
+    sodium = models.FloatField(default=0)               #mg
+
+    #Vitamins per 100g
+    #-------------------------------------
+    alpha_carotene = models.FloatField(default=0)       #μg
+    beta_carotene = models.FloatField(default=0)        #μg
+    choline = models.FloatField(default=0)              #mg
+    folate = models.FloatField(default=0)               #μg
+    lycopene = models.FloatField(default=0)             #μg
+    niacin = models.FloatField(default=0)               #mg
+    pantothenic_acid = models.FloatField(default=0)     #mg
+    retinol = models.FloatField(default=0)              #μg
+    riboflavin = models.FloatField(default=0)           #mg
+    vitamin_A_IU = models.FloatField(default=0)         #IU
+    vitamin_A = models.FloatField(default=0)            #μg
+    vitamin_B12 = models.FloatField(default=0)          #μg
+    vitamin_B6 = models.FloatField(default=0)           #mg
+    vitamin_C = models.FloatField(default=0)            #mg
+    vitamin_D_IU = models.FloatField(default=0)         #IU
+    vitamin_D = models.FloatField(default=0)            #μg
+    vitamin_D2 = models.FloatField(default=0)           #μg
+    vitamin_D3 = models.FloatField(default=0)           #μg
+    vitamin_E = models.FloatField(default=0)            #mg
+    vitamin_K = models.FloatField(default=0)            #μg
+
+    #Minerals per 100g
+    #-------------------------------------
+    caffeine = models.FloatField(default=0)             #mg
+    copper = models.FloatField(default=0)               #mg
+    flouride = models.FloatField(default=0)             #μg
+    magnesium = models.FloatField(default=0)            #mg
+    manganese = models.FloatField(default=0)            #mg
+    phosphorus = models.FloatField(default=0)           #mg
+    selenium = models.FloatField(default=0)             #μg
+    theobromine = models.FloatField(default=0)          #mg
+    zinc = models.FloatField(default=0)                 #mg
+    potassium = models.FloatField(default=0)            #mg
+    calcium = models.FloatField(default=0)              #mg
+    iron = models.FloatField(default=0)                 #mg
+
+    #Sugars per 100g
+    #-------------------------------------
+    sugar = models.FloatField(default=0)                #g
+    sucrose = models.FloatField(default=0)              #g
+    glucose = models.FloatField(default=0)              #g
+    fructose = models.FloatField(default=0)             #g
+    lactose = models.FloatField(default=0)              #g
+    maltose = models.FloatField(default=0)              #g
+    galactose = models.FloatField(default=0)            #g
+    starch = models.FloatField(default=0)               #g
+
+    #Fats per 100g
+    #-------------------------------------
+    saturated_fats = models.FloatField(default=0)       #g
+    monosaturated_fats = models.FloatField(default=0)   #g
+    polyunsaturated_fats = models.FloatField(default=0) #g
+    trans_fats = models.FloatField(default=0)           #g
+
+    #Fatty Acids per 100g
+    #-------------------------------------
+    omega_3 = models.FloatField(default=0)              #g  
+    omega_6 = models.FloatField(default=0)              #g
+    ALA = models.FloatField(default=0)                  #g
+    DHA = models.FloatField(default=0)                  #g
+    EPA = models.FloatField(default=0)                  #g
+    DPA = models.FloatField(default=0)                  #g
+
+    #Amino Acids per 100g
+    #-------------------------------------
+    alanine = models.FloatField(default=0)              #g
+    arginine = models.FloatField(default=0)             #g
+    aspartic_acid = models.FloatField(default=0)        #g
+    cystine = models.FloatField(default=0)              #g
+    glutamic_acid = models.FloatField(default=0)        #g
+    glycine = models.FloatField(default=0)              #g
+    histidine = models.FloatField(default=0)            #g
+    hydroxyproline = models.FloatField(default=0)       #g
+    isoleucine = models.FloatField(default=0)           #g
+    leucine = models.FloatField(default=0)              #g
+    lysine = models.FloatField(default=0)               #g
+    methionine = models.FloatField(default=0)           #g
+    phenylalanine = models.FloatField(default=0)        #g
+    proline = models.FloatField(default=0)              #g
+    serine = models.FloatField(default=0)               #g
+    threonine = models.FloatField(default=0)            #g
+    tryptophan = models.FloatField(default=0)           #g
+    tyrosine = models.FloatField(default=0)             #g
+    valine = models.FloatField(default=0)               #g
+
+    def __str__(self):
+        return self.name
+    
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='ingredients'
+    )
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE
+    )
+    amount = models.CharField(
+        max_length=20,
+        default='g'
+    )
+    class Meta:
+        unique_together = ['recipe', 'ingredient']
+    
+    def __str__(self):
+        return f"{self.recipe.name} - {self.ingredient.name}"
+    
+class Nutrition(models.Model):
+    recipe = models.OneToOneField(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='nutrition'
+    )
+    
+    #Macros per serving
+    #-------------------------------------
+    calories = models.FloatField(default=0)
+    fat = models.FloatField(default=0)                  #g
+    protein = models.FloatField(default=0)              #g
+    carbohydrates = models.FloatField(default=0)        #g
+    cholesterol = models.FloatField(default=0)          #mg
+    sodium = models.FloatField(default=0)               #mg
+
+    #Vitamins per serving
+    #-------------------------------------
+    alpha_carotene = models.FloatField(default=0)       #μg
+    beta_carotene = models.FloatField(default=0)        #μg
+    choline = models.FloatField(default=0)              #mg
+    folate = models.FloatField(default=0)               #μg
+    lycopene = models.FloatField(default=0)             #μg
+    niacin = models.FloatField(default=0)               #mg
+    pantothenic_acid = models.FloatField(default=0)     #mg
+    retinol = models.FloatField(default=0)              #μg
+    riboflavin = models.FloatField(default=0)           #mg
+    vitamin_A_IU = models.FloatField(default=0)         #IU
+    vitamin_A = models.FloatField(default=0)            #μg
+    vitamin_B12 = models.FloatField(default=0)          #μg
+    vitamin_B6 = models.FloatField(default=0)           #mg
+    vitamin_C = models.FloatField(default=0)            #mg
+    vitamin_D_IU = models.FloatField(default=0)         #IU
+    vitamin_D = models.FloatField(default=0)            #μg
+    vitamin_D2 = models.FloatField(default=0)           #μg
+    vitamin_D3 = models.FloatField(default=0)           #μg
+    vitamin_E = models.FloatField(default=0)            #mg
+    vitamin_K = models.FloatField(default=0)            #μg
+
+    #Minerals per serving
+    #-------------------------------------
+    caffeine = models.FloatField(default=0)             #mg
+    copper = models.FloatField(default=0)               #mg
+    flouride = models.FloatField(default=0)             #μg
+    magnesium = models.FloatField(default=0)            #mg
+    manganese = models.FloatField(default=0)            #mg
+    phosphorus = models.FloatField(default=0)           #mg
+    selenium = models.FloatField(default=0)             #μg
+    theobromine = models.FloatField(default=0)          #mg
+    zinc = models.FloatField(default=0)                 #mg
+    potassium = models.FloatField(default=0)            #mg
+    calcium = models.FloatField(default=0)              #mg
+    iron = models.FloatField(default=0)                 #mg
+
+    #Sugars per serving
+    #-------------------------------------
+    sugar = models.FloatField(default=0)                #g
+    sucrose = models.FloatField(default=0)              #g
+    glucose = models.FloatField(default=0)              #g
+    fructose = models.FloatField(default=0)             #g
+    lactose = models.FloatField(default=0)              #g
+    maltose = models.FloatField(default=0)              #g
+    galactose = models.FloatField(default=0)            #g
+    starch = models.FloatField(default=0)               #g
+
+    #Fats per serving
+    #-------------------------------------
+    saturated_fats = models.FloatField(default=0)       #g
+    monosaturated_fats = models.FloatField(default=0)   #g
+    polyunsaturated_fats = models.FloatField(default=0) #g
+    trans_fats = models.FloatField(default=0)           #g
+
+    #Fatty Acids per serving
+    #-------------------------------------
+    omega_3 = models.FloatField(default=0)              #g  
+    omega_6 = models.FloatField(default=0)              #g
+    ALA = models.FloatField(default=0)                  #g
+    DHA = models.FloatField(default=0)                  #g
+    EPA = models.FloatField(default=0)                  #g
+    DPA = models.FloatField(default=0)                  #g
+
+    #Amino Acids per serving
+    #-------------------------------------
+    alanine = models.FloatField(default=0)              #g
+    arginine = models.FloatField(default=0)             #g
+    aspartic_acid = models.FloatField(default=0)        #g
+    cystine = models.FloatField(default=0)              #g
+    glutamic_acid = models.FloatField(default=0)        #g
+    glycine = models.FloatField(default=0)              #g
+    histidine = models.FloatField(default=0)            #g
+    hydroxyproline = models.FloatField(default=0)       #g
+    isoleucine = models.FloatField(default=0)           #g
+    leucine = models.FloatField(default=0)              #g
+    lysine = models.FloatField(default=0)               #g
+    methionine = models.FloatField(default=0)           #g
+    phenylalanine = models.FloatField(default=0)        #g
+    proline = models.FloatField(default=0)              #g
+    serine = models.FloatField(default=0)               #g
+    threonine = models.FloatField(default=0)            #g
+    tryptophan = models.FloatField(default=0)           #g
+    tyrosine = models.FloatField(default=0)             #g
+    valine = models.FloatField(default=0)               #g
+
+    def __str__(self):
+        return f"{self.recipe.name} Nutrition"
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
 # Create your views here.
+recipes = [
+    {
+        'id': 1, 'name': 'Breakfast Sourdough Sandwich', 'description': 'A sourdough sandwich with steamed egg over avacodo with hot sauce and black pepper sprinkled on.',
+        'steps': {'1': 'Bring a pan to medium heat', '2': 'Spray pan with cooking oil', '3': 'Put sourdough slices into toaster set to preferred toast', '4': 'Once pan is hot, crack egg into pan', '5': 'Pour a little bit of water into pan and place a cover over the pan', '6': 'Once sourdough is toasted, carve out avacodo and spread it onto one slice of bread', '7': 'Take egg out of pan and lay it over the avacodo', '8': 'Sprinkle hot sauce and pepper over egg', '9': 'Put other slice on top', },
+        'macros': {'fat': '36.2g', 'protein': '18g', 'carb': '54.5g', 'cholesterol': '186mg', 'sodium': '595mg', },
+        'vitamins': {'alpha_carotene': '49.3μg', 'beta_carotene': '149μg', 'choline': '182mg', 'folate': '267.3μg', 'lycopene': '1μg', 'niacin': '6.1mg', 'pantothenic_acid': '4.4mg', 'retinol': '80μg', 'riboflavin': '0.8mg', 'vitamin_A_IU': '606iu', 'vitamin_A': '96.4μg', 'vitamin_B12': '0.4μg', 'vitamin_B6': '1.2mg', 'vitamin_C': '24mg', 'vitamin_D_iu': '41iu', 'vitamin_D': '1μg', 'vitamin_D2': '', 'vitamin_D3': '1μg', 'vitamin_E': '5.2mg', 'vitamin_K': '52.7μg', },
+        'minerals': {'caffeine': '0mg', 'copper': '0.6mg', 'flouride': '17μg', 'magnesium': '95.2mg', 'manganese': '1.7mg', 'phosphorus': '282mg', 'selenium': '34.3μg', 'theobromine': '0mg', 'zinc': '3.1mg', 'potassium': '1211mg', 'calcium': '113.4mg', 'iron': '6mg', },
+        'sugars': {'sugar': '4.3g', 'sucrose': '0.1g', 'glucose': '1.5g', 'fructose': '0.5g', 'lactose': '0g', 'maltose': '2g', 'galactose': '0.2g', 'starch': '28.2g', },
+        'fats': {'saturated_fats': '6.4g', 'monounsaturated_fats': '22.2g', 'polyunsaturated_fats': '6.1g', 'trans_fats': '0g', },
+        'fatty_acids': {'omega_3': '0.3g', 'omega_6': '2g', 'ala': '0.2g', 'dha': '0g', 'epa': '0g', 'dpa': '0g', },
+        'amino_acids': {'alanine': '0.8g', 'arginine': '0.8g', 'aspartic_acid': '1.9g', 'cystine': '0.4g', "glutamic_acid": '4.1g', 'glycine': '0.6g', 'histidine': '0.4g', 'hydroxyproline': '', 'isoleucine': '0.7g', 'leucine': '1.7g', 'lysine': '1g', 'methionine': '0.4g', 'phenylalanine': '0.8g', 'proline': '1.6g', 'serine': '1g', 'threonine': '0.6g', 'tryptophan': '0.3g', 'tyrosine': '0.4g', 'valine': '0.9g', },
+        'serving_size': '1 sandwich',
+        'calories': '585',
+        'source': '',
+    },
+]
+def index(request):
+    template_data = {}
+    template_data['title'] = 'Recipes'
+    template_data['recipes'] = recipes
+    return render(request, 'recipes/index.html', {'template_data': template_data})
+def show(request, id):
+    recipe = recipes[id - 1]
+    template_data = {}
+    template_data['title'] = recipe['name']
+    template_data['recipe'] = recipe
+    return render(request, 'recipes/show.html', {'template_data': template_data})
